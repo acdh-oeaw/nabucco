f6b53def0c95e8fa563f428d3efc804b04124757
pirnr81 <reinhard.pirngruber@gmail.com>
2022-10-21 11:39:52 +0200

    about the project bug

--- djangobaseproject/urls.py
+++ djangobaseproject/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^browsing/', include('browsing.urls', namespace='browsing')),
     url(r'^netvis/', include('netvis.urls', namespace="netvis")),
-    url(r'^info/', include('infos.urls', namespace='infos')),
+    url(r'^info/', include('infos.urls', namespace='info')),
     url(r'^archiv/', include('archiv.urls', namespace='archiv')),
     url(r'^archiv-ac/', include('archiv.dal_urls', namespace='archiv-ac')),
     url(r'^', include('webpage.urls', namespace='webpage')),
