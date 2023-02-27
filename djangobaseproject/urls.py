0789c2e95eec66f06e27c96c045d593c7f61e6ae
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-27 12:46:39 +0100

    wip: fixing url vs path issues #26

--- djangobaseproject/urls.py
+++ djangobaseproject/urls.py
@@ -1,15 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^browsing/', include('browsing.urls', namespace='browsing')),
-    url(r'^netvis/', include('netvis.urls', namespace="netvis")),
-    url(r'^info/', include('infos.urls', namespace='info')),
-    url(r'^archiv/', include('archiv.urls', namespace='archiv')),
-    url(r'^archiv-ac/', include('archiv.dal_urls', namespace='archiv-ac')),
-    url(r'^', include('webpage.urls', namespace='webpage')),
+    path("admin/", admin.site.urls),
+    path("browsing/", include("browsing.urls", namespace="browsing")),
+    path("netvis/", include("netvis.urls", namespace="netvis")),
+    path("info/", include("infos.urls", namespace="info")),
+    path("archiv/", include("archiv.urls", namespace="archiv")),
+    path("archiv-ac/", include("archiv.dal_urls", namespace="archiv-ac")),
+    path("", include("webpage.urls", namespace="webpage")),
 ]
