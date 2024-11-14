a03976934af83e210f004f3c7e78cc6b36af7581
csae8092 <p.andorfer@gmail.com>
2024-11-14 09:50:45 +0100

    fix for start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+# start-server.sh
 echo "Hello from Project Nabucco"
 python manage.py collectstatic --no-input
-echo "running any pending migrations"
+echo "running migrations"
+python manage.py migrate --no-input
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && python manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
 fi
