5d1058754b6491916d1ec981fdfa87c78fe72385
csae8092 <p.andorfer@gmail.com>
2025-01-16 12:47:38 +0100

    adds tests for news views

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,45 @@
-# from django.test import TestCase
+from django.apps import apps
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+MODELS = list(apps.all_models["news"].values())
+to_check = [x for x in MODELS]
+
+client = Client()
+USER = {"username": "testuser", "password": "somepassword"}
+
+
+class NewsTestCase(TestCase):
+    fixtures = ["dump.json"]
+
+    def setUp(self):
+        User.objects.create_user(**USER)
+
+    def test_002_listviews(self):
+        for x in to_check:
+            url = x.get_listview_url()
+            response = client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_003_detailviews(self):
+        for x in to_check:
+            item = x.objects.first()
+            url = item.get_absolute_url()
+            response = client.get(url, {"pk": item.id})
+            self.assertEqual(response.status_code, 200)
+
+    def test_004_editviews(self):
+        client.login(**USER)
+        for x in to_check:
+            item = x.objects.first()
+            url = item.get_edit_url()
+            response = client.get(url, {"pk": item.id})
+            self.assertEqual(response.status_code, 200)
+
+    def test_005_createviews_not_logged_in(self):
+        for x in to_check:
+            item = x.objects.first()
+            url = item.get_createview_url()
+            response = client.get(url, {"pk": item.id})
+            self.assertEqual(response.status_code, 302)
