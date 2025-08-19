f8a808d87101f106abc3162ec26ae1fbbcb2ebad
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2025-08-19 09:19:36 +0200

    135 chore switch to uv (#136)
    * chore: adds pyproject.toml; adapting test.yml
    * chore: us uv in Docker set up

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from nabucco!")
+
+
+if __name__ == "__main__":
+    main()
--- start-server.sh
+++ start-server.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# start-server.sh
-echo "Hello from Project Nabucco"
-python manage.py collectstatic --no-input
+echo "Hello from Project nabucco"
+uv run manage.py collectstatic --no-input
 echo "running migrations"
-python manage.py migrate --no-input
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && python manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
-fi
-gunicorn djangobaseproject.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
+uv run manage.py migrate --no-input
+uv run gunicorn djangobaseproject.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
