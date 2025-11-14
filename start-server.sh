66a537f2e0a61dab35c8d79e5092c56e73363199
csae8092 <p.andorfer@gmail.com>
2025-11-14 09:15:48 +0100

    fix: typo [skip ci]

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@ echo "Hello from Project nabucco"
 uv run manage.py collectstatic --no-input
 echo "running migrations"
 uv run manage.py migrate --no-input
-uv uv run manage.py prune_no_author_logs  
+uv run manage.py prune_no_author_logs  
 uv run gunicorn djangobaseproject.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
