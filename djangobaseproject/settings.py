1ff3de716990288c493905f7b6eb2249977dea2f
csae8092 <p.andorfer@gmail.com>
2026-03-02 10:54:05 +0100

    feat: nippur landing page

--- djangobaseproject/settings.py
+++ djangobaseproject/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     "archiv",
     "digeanna",
     "navico",
+    "nippur",
     "tinymce",
     "news",
     "fixture_magic",
---
+++ nippur/views.py
@@ -0,0 +1,24 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic import TemplateView
+
+from infos.models import AboutTheProject
+from news.models import NewsEntry
+
+
+class IndexView(TemplateView):
+    template_name = "nippur/index.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["news"] = NewsEntry.objects.all()[:3]
+        return super().get_context_data(**kwargs)
+
+
+class About(TemplateView):
+    template_name = "nippur/about.html"
+
+    def get_context_data(self, **kwargs):
+        try:
+            kwargs["object"] = AboutTheProject.objects.get(title="Nippur")
+        except ObjectDoesNotExist:
+            kwargs["object"] = {"error": True}
+        return super().get_context_data(**kwargs)
