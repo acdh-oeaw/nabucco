62f1d6c62fff4a00dda22212dfeabcbf1d449cb1
csae8092 <p.andorfer@gmail.com>
2026-03-06 15:02:46 +0100

    fix: remove default value for secret key

--- djangobaseproject/settings.py
+++ djangobaseproject/settings.py
@@ -33,7 +33,7 @@ SHEET_ID = os.environ.get("SHEET_ID", "19tfPPMRiIQf6SPicfhvX3gMQd0Hfb5I6eEeKg2xw
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "üpxyd-dsafjlsfj-wy5^_*oo3sac4e6")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DEBUG"):
