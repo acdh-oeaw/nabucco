3e0951b666ab81e3ab87e32d13d02da578441cf8
csae8092 <p.andorfer@gmail.com>
2026-08-19 15:25:34 +0200

    feat(dasboard): render all possible fields

--- stats/urls.py
+++ stats/urls.py
@@ -5,5 +5,5 @@ from stats import views
 app_name = "network"
 urlpatterns = [
     path("tablet-stats-data/", views.tablet_stats_data, name="tablet-stats-data"),
-    path("tablet-dashboard/", views.TabletDashboard.as_view(), name="tablet-dasboard"),
+    path("tablet-dashboard/", views.TabletDashboard.as_view(), name="tablet-dashboard"),
 ]
